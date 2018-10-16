348710ca00c757f173198a9fca26b49f0e679815
Mient-jan Stelling <mient-jan@mediamonks.com>
2018-10-16 14:15:00 +0200

    adding requires to RefsComponent

--- src/component/RefsComponent.js
+++ src/component/RefsComponent.js
@@ -2,26 +2,24 @@ import Component from './Component';
 import ElementComponent from './ElementComponent';
 
 export default class RefsComponent extends Component {
-  refs = {};
+  static requires = [ElementComponent];
 
-  constructor() {
-    super();
-  }
+  refs = {};
 
   onStart() {
     const element = this.getComponent(ElementComponent).get();
-    const refs = this.refs;
+    const { refs } = this;
     const elements = element.querySelectorAll('[ref]');
 
     for (let i = 0; i < elements.length; i++) {
-      const element = elements[i];
-      const name = element.getAttribute('ref');
+      const el = elements[i];
+      const name = el.getAttribute('ref');
 
       if (!refs[name]) {
         refs[name] = [];
       }
 
-      refs[name].push(element);
+      refs[name].push(el);
     }
   }
 

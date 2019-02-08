fc1438e21b03700facec145ea742767d98dcc117
mm-paulie <paulie@mediamonks.com>
2019-02-08 14:28:08 +0100

    updated findelements with lowercase instead of uppercase

--- src/util/findElements.js
+++ src/util/findElements.js
@@ -1,11 +1,10 @@
 export default function findElements(e, styles, customTypes, sheet) {
   return new Promise(function(resolve) {
-
-    var arr = ["DIV", "SPAN", "IMG", "CANVAS", "SVG", "CIRCLE", "PATH"];
-    arr = arr.concat(customTypes)
+    var arr = ['div', 'span', 'img', 'canvas', 'svg', 'circle', 'path'];
+    arr = arr.concat(customTypes);
 
     if (styles) {
-      var obj = {}
+      var obj = {};
       obj.all = [];
       findElement(e, styles, customTypes, sheet);
     } else {
@@ -17,10 +16,10 @@ export default function findElements(e, styles, customTypes, sheet) {
       if (e && e.childNodes && e.childNodes.length > 0) {
         for (var i = 0; i < e.childNodes.length; i++) {
           var child = e.childNodes[i];
-          if (child.type == "image/svg+xml" || arr.indexOf(child.nodeName.toUpperCase()) != -1) {
+          if (child.type == 'image/svg+xml' || arr.indexOf(child.nodeName.toLowerCase()) != -1) {
             if (child.id || child.className) {
               if (styles) {
-                styles = (typeof(styles) == "string") ? [styles] : styles;
+                styles = typeof styles == 'string' ? [styles] : styles;
 
                 for (var j = 0; j < styles.length; j++) {
                   if (!obj[styles[j]]) {
@@ -28,17 +27,19 @@ export default function findElements(e, styles, customTypes, sheet) {
                   }
 
                   if (child.id && obj[styles[j]].indexOf(child) == -1) {
-                    var val = getStyleRuleValue("." + styles[j], "#" + child.id, sheet);
+                    var val = getStyleRuleValue('.' + styles[j], '#' + child.id, sheet);
                     if (val) {
                       obj[styles[j]].push(child);
                     }
                   }
-                  var c = (typeof(child.className) == "object") ? String(child.className.baseVal).split(" ") : String(child.className).split(" ");
+                  var c =
+                    typeof child.className == 'object'
+                      ? String(child.className.baseVal).split(' ')
+                      : String(child.className).split(' ');
 
                   for (var k = 0; k < c.length; k++) {
-
                     if (c[k] && obj[styles[j]].indexOf(child) == -1) {
-                      var val = getStyleRuleValue("." + styles[j], "." + c[k], sheet);
+                      var val = getStyleRuleValue('.' + styles[j], '.' + c[k], sheet);
                       if (val) {
                         obj[styles[j]].push(child);
                       }
@@ -60,20 +61,27 @@ export default function findElements(e, styles, customTypes, sheet) {
     function getStyleRuleValue(style, selector, sheet) {
       var sheets = [];
       for (var k in document.styleSheets) {
-        if (document.styleSheets[k].href && (document.styleSheets[k].href.indexOf("style.css") != -1 || document.styleSheets[k].href.indexOf(sheet) != -1)) {
+        if (
+          document.styleSheets[k].href &&
+          (document.styleSheets[k].href.indexOf('style.css') != -1 ||
+            document.styleSheets[k].href.indexOf(sheet) != -1)
+        ) {
           sheets.push(document.styleSheets[k]);
         }
       }
 
-      var ar = []
+      var ar = [];
       for (var i = 0, l = sheets.length; i < l; i++) {
         var sheet = sheets[i];
         if (!sheet.cssRules) continue;
         for (var j = 0, k = sheet.cssRules.length; j < k; j++) {
           var rule = sheet.cssRules[j];
           if (rule.selectorText) {
-            if (rule.selectorText.indexOf(selector) != -1 && rule.selectorText.indexOf(style) != -1) {
-              var all = rule.selectorText.substring(0, rule.selectorText.indexOf(style)).split(".");
+            if (
+              rule.selectorText.indexOf(selector) != -1 &&
+              rule.selectorText.indexOf(style) != -1
+            ) {
+              var all = rule.selectorText.substring(0, rule.selectorText.indexOf(style)).split('.');
               var node = all[all.length - 1];
               return node;
             }
@@ -83,10 +91,5 @@ export default function findElements(e, styles, customTypes, sheet) {
       return;
     }
     resolve(obj);
-
-
-
-
-
   });
 }

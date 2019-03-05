3c83d806601270c7d021d5a3be2e8ade238777c9
mm-paulie <paulie@mediamonks.com>
2019-03-05 13:06:44 +0100

    updated files findElementsByCSS updated files findElementsByCSS with customType so that it can search within custom components and elements (cherry picked from commit de2ed8f612668f302c738f521f9685abc0716b4f)

--- src/util/findElementByCSS.js
+++ src/util/findElementByCSS.js
@@ -1,21 +1,31 @@
 import getStyleRuleValue from './getStyleRuleValue';
 
-const elementsToSearch = ['DIV', 'SPAN', 'IMG', 'CANVAS', 'SVG', 'CIRCLE', 'PATH'];
+let elementsToSearch = ['div', 'span', 'img', 'canvas', 'svg', 'circle', 'path'];
 
+/**
+ *
+ * @param {HTMLElement} element
+ * @param {Array<string>} styles
+ * @param {string} sheet
+ * @param {object} obj
+ * @return {{all: Array}}
+ */
 export default function findElementByCSS(
   element,
-  styles,
-  sheet,
+  styles = null,
+  customTypes = null,
+  sheet = null,
   obj = {
     all: [],
   },
 ) {
+  elementsToSearch = elementsToSearch.concat(customTypes);
   if (element && element.childNodes && element.childNodes.length > 0) {
     for (let i = 0; i < element.childNodes.length; i++) {
       const child = element.childNodes[i];
       if (
         child.type === 'image/svg+xml' ||
-        elementsToSearch.indexOf(child.nodeName.toUpperCase()) !== -1
+        elementsToSearch.indexOf(child.nodeName.toLowerCase()) !== -1
       ) {
         if (child.id || child.className) {
           if (styles) {
@@ -27,31 +37,31 @@ export default function findElementByCSS(
               }
 
               if (child.id && obj[styles[j]].indexOf(child) === -1) {
-                var val = getStyleRuleValue('.' + styles[j], '#' + child.id, sheet);
+                const val = getStyleRuleValue(`.${styles[j]}`, `#${child.id}`, sheet);
                 if (val) {
                   obj[styles[j]].push(child);
                 }
               }
 
-              const c =
+              const cssClasses =
                 typeof child.className === 'object'
                   ? String(child.className.baseVal).split(' ')
                   : String(child.className).split(' ');
 
-              for (let k = 0; k < c.length; k++) {
-                if (c[k] && obj[styles[j]].indexOf(child) === -1) {
-                  const val = getStyleRuleValue('.' + styles[j], '.' + c[k], sheet);
-                  if (val) {
+              for (let k = 0; k < cssClasses.length; k++) {
+                if (cssClasses[k] && obj[styles[j]].indexOf(child) === -1) {
+                  const val = getStyleRuleValue(`.${styles[j]}`, `.${cssClasses[k]}`, sheet);
+                  if (val && cssClasses[k] == val) {
                     obj[styles[j]].push(child);
                   }
                 }
               }
             }
             obj.all.push(child);
-            findElementByCSS(child, styles, sheet, obj);
+            findElementByCSS(child, styles, null, sheet, obj);
           } else {
             obj.push(child);
-            findElementByCSS(child, null, null, obj);
+            findElementByCSS(child, null, null, null, obj);
           }
         }
       }
--- src/util/findElementsByCSS.js
+++ src/util/findElementsByCSS.js
@@ -1,9 +1,9 @@
 import findElementByCSS from './findElementByCSS';
 
-export default function findElementsByCSS(element, styles, sheet) {
-  var obj;
+export default function findElementsByCSS(element, styles, customTypes, sheet) {
+  let obj;
   if (styles) {
-    obj = findElementByCSS(element, styles, sheet);
+    obj = findElementByCSS(element, styles, customTypes, sheet);
   } else {
     obj = findElementByCSS(element);
   }

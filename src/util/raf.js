219afdaa2f5f856111a3b95f363623afe9fe277f
Mient-jan Stelling <mient-jan@mediamonks.com>
2019-03-06 14:16:26 +0100

    fixing raf now passing time to it.

--- src/util/raf.js
+++ src/util/raf.js
@@ -1,22 +1,22 @@
 export default function raf(callback, limitFps = -1) {
   let rafNum = -1;
-  let totalTime = 0;
+  let currentTime = 0;
   const hasLimiter = limitFps > 0;
   const limitMsps = 1000 / limitFps;
   let timeAcc = limitMsps;
 
   const tick = timestamp => {
     rafNum = window.requestAnimationFrame(tick);
-    if (!hasLimiter) {
-      callback.call();
-    } else if (!totalTime) {
-      totalTime = timestamp;
-    } else {
-      timeAcc += timestamp - totalTime;
-      totalTime = timestamp;
+    const timeDiff = timestamp - currentTime;
+    currentTime = timestamp;
+
+    if (timeDiff > 0 && !hasLimiter) {
+      callback.call(null, timestamp, timeDiff);
+    } else if (timeDiff > 0) {
+      timeAcc += timeDiff;
       while (timeAcc > limitMsps) {
         timeAcc -= limitMsps;
-        callback.call();
+        callback.call(null, timestamp, limitMsps);
       }
     }
   };

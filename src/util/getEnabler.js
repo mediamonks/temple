dcb98b859193351692950cc9d620884fba2d9108
Mient-jan Stelling <mient-jan@mediamonks.com>
2019-03-12 15:20:24 +0100

    removing init on studio

--- src/util/getEnabler.js
+++ src/util/getEnabler.js
@@ -1,25 +1,8 @@
 let prom = null;
-let promInitialized = null;
 const limit = 10000;
 let count = 0;
 
-export default function getEnabler(isInitialized = true) {
-  if (isInitialized) {
-    if (!promInitialized) {
-      promInitialized = getEnabler(false).then(Enabler => {
-        return new Promise(resolve => {
-          const resolver = () => {
-            Enabler.removeEventListener(studio.events.StudioEvent.INIT, resolver);
-            resolve(Enabler);
-          };
-          Enabler.addEventListener(studio.events.StudioEvent.INIT, resolver);
-        });
-      });
-    }
-
-    return promInitialized;
-  }
-
+export default function getEnabler() {
   if (!prom) {
     prom = new Promise((resolve, reject) => {
       const checkLoop = function() {

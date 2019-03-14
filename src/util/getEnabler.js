242433bc34ae88653afc6edd0a515f2390188dfc
Mient-jan Stelling <mient-jan@mediamonks.com>
2019-03-14 15:10:13 +0100

    fixing getEnabler

--- src/util/getEnabler.js
+++ src/util/getEnabler.js
@@ -1,9 +1,23 @@
-let prom = null;
+let promise = null;
 let promInitialized = null;
 const limit = 10000;
 let count = 0;
 
-export default function getEnabler(isInitialized = true) {
+export default async function getEnabler(isInitialized = true) {
+  if (!window.Enabler) {
+    await new Promise(resolve => {
+      const checkLoop = function() {
+        if (window.Enabler) {
+          resolve(window.Enabler);
+        } else {
+          setTimeout(checkLoop, 500);
+        }
+      };
+
+      checkLoop();
+    });
+  }
+
   if (isInitialized) {
     if (!promInitialized) {
       promInitialized = getEnabler(false).then(Enabler => {
@@ -20,8 +34,8 @@ export default function getEnabler(isInitialized = true) {
     return promInitialized;
   }
 
-  if (!prom) {
-    prom = new Promise((resolve, reject) => {
+  if (!promise) {
+    promise = new Promise((resolve, reject) => {
       const checkLoop = function() {
         if (!!Enabler) {
           resolve(Enabler);

7b007bcd646b8752266c4b96e9e1d1e2ed2970e8
Mient-jan Stelling <mient-jan@mediamonks.com>
2020-08-31 16:26:00 +0200

    adding svg check

--- src/component/EmbedSVGComponent.js
+++ src/component/EmbedSVGComponent.js
@@ -13,6 +13,12 @@ export default class EmbedSVGComponent extends Component {
       const element = this.getComponent(ElementComponent).get();
       const elements = Array.from(element.querySelectorAll('[svg]'));
 
+      elements.forEach(img => {
+        if (!img.src) {
+          throw new Error('Missing src attribute');
+        }
+      });
+
       return Promise.all(elements.map(img => loadText(img.src))).then(svgList => {
         svgList.forEach((svgText, index) => {
           const el = elements[index];
